b455070766fdd6b67968dfc405425d0f5e4c20f8
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-08-24 13:39:04 -0700

    video search works

--- src/index.js
+++ src/index.js
@@ -15,20 +15,24 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
-    
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
-    //  same as this.setState({videos: videos})
+      //  same as this.setState({videos: videos})
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={(term) => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
